31ac1e914ded173131851a956cf884c85b451dc6
kamilrextin <kamil.rextin@gmail.com>
2015-04-04 12:03:07 -0400

    more ex

---
+++ ex11.rb
@@ -0,0 +1,12 @@
+print "How old are you?"
+age = gets.chomp
+print "how tall are you?"
+height = gets.chomp
+print "What is your weight"
+weight = gets.chomp
+
+puts "so you are #{age} and #{height} and #{weight}."
+
+puts "is that correct"
+ans = gets
+puts "thanks your answer is #{ans}"
