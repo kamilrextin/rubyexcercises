c07c20a121e4f958305d5ccdfed9bf1a0ac20bae
kamilrextin <kamil.rextin@gmail.com>
2015-04-05 16:59:50 -0400

    floats and integers

--- ex12.rb
+++ ex12.rb
@@ -6,7 +6,9 @@ puts "a bigger number is #{bigger}"
 
 print "give me another number: "
 another = gets.chomp
-number = another.to_i
+number1 = another.to_i
+number2 = number1.to_f
+puts number2
 
 smaller = number / 100
 puts "a smaller number is #{smaller}"
