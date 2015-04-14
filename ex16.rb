7a26c9e0a74f6fe8c20efc677211b2f05056fabe
kamilrextin <kamil.rextin@gmail.com>
2015-04-14 16:39:42 -0400

    commenting and now it works.... weird

--- ex16.rb
+++ ex16.rb
@@ -1,28 +1,32 @@
+# grab file name from command line and put it into filename variable
 filename = ARGV.first
 
 puts "we;re going to erase #{filename}"
 puts "If you dont want that, hit CRTC C"
 puts "if you do want that hit return"
 
+#get input from keyboard (explicitly from the user)
 $stdin.gets
 
 puts "Opening the file ... "
 
-target = open (filename, 'w')
+#save file state into Target variable?
+target = open(filename, 'w')
 
 puts "truncating the file. Goodbye!"
+# delete file contents.
 target.truncate(0)
 
 puts "Now I am going to ask you three lines"
 
-print "line 1"
+print "line 1: "
 line1 = $stdin.gets.chomp
-print "line 2"
+print "line 2:"
 line2 = $stdin.gets.chomp
-print "line 3"
+print "line 3:"
 line3 = $stdin.gets.chomp
 
-puts "I am going to write these into the file"
+puts "I am going to write these into the file."
 
 target.write(line1)
 target.write("\n")
@@ -31,5 +35,5 @@ target.write("\n")
 target.write(line3)
 target.write("\n")
 
-puts "And finally close it"
+puts "And finally close it."
 target.close
